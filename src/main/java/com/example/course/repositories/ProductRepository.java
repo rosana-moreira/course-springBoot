1c6e1dd3efff486d5026b5c674cf6e9535a2fc29
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-07-04 15:21:06 -0300

    Product

---
+++ src/main/java/com/example/course/repositories/ProductRepository.java
@@ -0,0 +1,1 @@
+package com.example.course.repositories;import com.example.course.entities.Product;import org.springframework.data.jpa.repository.JpaRepository;public interface ProductRepository extends JpaRepository<Product, Long> {}
