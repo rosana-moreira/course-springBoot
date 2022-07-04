883c499cbb2b588aa48b9e0a23cc6992c99eaf46
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-07-04 14:52:53 -0300

    Category

---
+++ src/main/java/com/example/course/entities/Product.java
@@ -0,0 +1,1 @@
+package com.example.course.entities;public class Product {}
---
+++ src/main/java/com/example/course/repositories/CategoryRepository.java
@@ -0,0 +1,1 @@
+package com.example.course.repositories;import com.example.course.entities.Category;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;@Repositorypublic interface CategoryRepository extends JpaRepository<Category, Long> {}
