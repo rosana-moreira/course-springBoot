725ea7e170623fa1ad3ff5a7946646581c7c4dd8
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-07-06 18:58:43 -0300

    Update OrderResource.java

--- src/main/java/com/example/course/resources/OrderResource.java
+++ src/main/java/com/example/course/resources/OrderResource.java
@@ -1,1 +1,32 @@
-package com.example.course.resources;import com.example.course.entities.Order;import com.example.course.services.OrderService;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.http.ResponseEntity;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.PathVariable;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RestController;import java.util.List;@RestController@RequestMapping(value = "/orders")public class OrderResource {    @Autowired    private OrderService orderService;    @GetMapping    public ResponseEntity<List> findAll() {        List<Order> list = orderService.findAll();        return ResponseEntity.ok().body(list);    }    @GetMapping(value = "/{id}")    public ResponseEntity<Order> findById(@PathVariable Long id) {        Order obj = orderService.findById(id);        return ResponseEntity.ok().body(obj);    }}
+package com.example.course.resources;
+
+import com.example.course.entities.Order;
+import com.example.course.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public ResponseEntity<List> findAll() {
+        List<Order> list = orderService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Order> findById(@PathVariable Long id) {
+        Order obj = orderService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+  
+}
