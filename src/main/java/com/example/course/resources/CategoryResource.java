7b9d33a805cb84ad35e3e8c1291dd0fdf95f1213
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-07-06 18:58:28 -0300

    Update CategoryResource.java

--- src/main/java/com/example/course/resources/CategoryResource.java
+++ src/main/java/com/example/course/resources/CategoryResource.java
@@ -1,1 +1,28 @@
-package com.example.course.resources;import com.example.course.entities.Category;import com.example.course.services.CategoryService;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.http.ResponseEntity;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.PathVariable;import java.util.List;public class CategoryResource {    @Autowired    private CategoryService categoryService;    @GetMapping    public ResponseEntity<List> findAll() {        List<Category> list = categoryService.findAll();        return ResponseEntity.ok().body(list);    }    @GetMapping(value = "/{id}")    public ResponseEntity<Category> findById(@PathVariable Long id) {        Category obj = categoryService.findById(id);        return ResponseEntity.ok().body(obj);    }}
+package com.example.course.resources;
+
+import com.example.course.entities.Category;
+import com.example.course.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+public class CategoryResource {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List> findAll() {
+        List<Category> list = categoryService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        Category obj = categoryService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+  
+}
