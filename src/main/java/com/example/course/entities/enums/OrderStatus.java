3b1b64afd5c21eb95ba94a57295137c612a3ca45
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-07-06 18:53:38 -0300

    Update OrderStatus.java

--- src/main/java/com/example/course/entities/enums/OrderStatus.java
+++ src/main/java/com/example/course/entities/enums/OrderStatus.java
@@ -1,1 +1,29 @@
-package com.example.course.entities.enums;public enum OrderStatus {    WAITING_PAYMENT(1),    PAID(2),    SHIPPED(3),    DELIVERED(4),    CANCELED(5);    private final int code;    OrderStatus(int code) {        this.code = code;    }    public static OrderStatus valueOf(int code) {        for (OrderStatus value : OrderStatus.values()) {            if (value.getCode() == code) {                return value;            }        }        throw new IllegalArgumentException("Invalid OrderStatus code");    }    public int getCode() {        return code;    }}
+package com.example.course.entities.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT(1),
+    PAID(2),
+    SHIPPED(3),
+    DELIVERED(4),
+    CANCELED(5);
+
+    private final int code;
+
+    OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus value : OrderStatus.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid OrderStatus code");
+    }
+
+    public int getCode() {
+        return code;
+    }
+  
+}
