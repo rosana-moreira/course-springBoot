0167c00b94d11b34ec1e34b5bffbd2de65b3a09f
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-07-06 18:59:47 -0300

    Update CategoryService.java

--- src/main/java/com/example/course/services/CategoryService.java
+++ src/main/java/com/example/course/services/CategoryService.java
@@ -1,1 +1,24 @@
-package com.example.course.services;import com.example.course.entities.Category;import com.example.course.repositories.CategoryRepository;import org.springframework.beans.factory.annotation.Autowired;import java.util.List;import java.util.Optional;public class CategoryService {    @Autowired    private CategoryRepository categoryRepository;    public List<Category> findAll() {        return categoryRepository.findAll();    }    public Category findById(Long id) {        Optional<Category> obj = categoryRepository.findById(id);        return obj.get();    }}
+package com.example.course.services;
+
+import com.example.course.entities.Category;
+import com.example.course.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        return obj.get();
+
+    }
+  
+}
