e11389237bc27d0d40ac684205aefd98c1373d1b
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-07-06 18:57:12 -0300

    Update UserRepository.java

--- src/main/java/com/example/course/repositories/UserRepository.java
+++ src/main/java/com/example/course/repositories/UserRepository.java
@@ -1,1 +1,10 @@
-package com.example.course.repositories;import com.example.course.entities.User;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;@Repositorypublic interface UserRepository extends JpaRepository<User, Long> {}
+package com.example.course.repositories;
+
+import com.example.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  
+}
