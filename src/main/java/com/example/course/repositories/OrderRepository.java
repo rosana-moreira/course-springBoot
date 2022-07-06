29a0fac642c9febd8f51bd9300fe64a27f3ac532
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-07-06 18:54:14 -0300

    Update OrderRepository.java

--- src/main/java/com/example/course/repositories/OrderRepository.java
+++ src/main/java/com/example/course/repositories/OrderRepository.java
@@ -1,1 +1,10 @@
-package com.example.course.repositories;import com.example.course.entities.Order;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;@Repositorypublic interface OrderRepository extends JpaRepository<Order, Long> {}
+package com.example.course.repositories;
+
+import com.example.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+  
+}
