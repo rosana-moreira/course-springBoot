8de7251a64cfa2d10a9e4277c628a31d784d2a95
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-07-06 19:00:33 -0300

    Update UserService.java

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -1,1 +1,26 @@
-package com.example.course.services;import com.example.course.entities.User;import com.example.course.repositories.UserRepository;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.stereotype.Service;import java.util.List;import java.util.Optional;@Servicepublic class UserService {    @Autowired    private UserRepository userRepository;    public List<User> findAll() {        return userRepository.findAll();    }    public User findById(Long id) {        Optional<User> obj = userRepository.findById(id);        return obj.get();    }}
+package com.example.course.services;
+
+import com.example.course.entities.User;
+import com.example.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+
+    }
+  
+}
